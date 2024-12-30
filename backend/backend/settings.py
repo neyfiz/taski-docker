d3e86b5c40613f178d14fc53dc1d5fa4c347f23d
Igor Gorelov <ney24fiz@gmail.com>
2024-12-30 16:30:06 +0300

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['neyfiz.zapto.org', 'localhost', '127.0.0.1']
 
 # Application definition
 
